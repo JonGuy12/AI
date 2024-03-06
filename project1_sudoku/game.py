6892d999ff07ed0bf4bd183cae402dd49ad54594
jonguy <jguy11230@gmail.com>
2024-03-06 17:25:01 -0500

    - Fixed DFS with pruning bug - Changed BFS from creating Sudoku objects to creating just boards - Pretty print results

--- project1_sudoku/game.py
+++ project1_sudoku/game.py
@@ -47,20 +47,20 @@ params: Takes the current puzzle as input
 Return: The puzzle board corresponding to the goal
 Note: You can modify the function definition as you see fit
 '''
-def bfs(puzzle):
+def bfs(puzzle, board):
     #Write Code here
     frontier = queue.Queue()
-    frontier.put(puzzle)
+    frontier.put(board)
 
     while not frontier.empty():
-        curr_board = frontier.get().board
+        curr_board = frontier.get()
         if test_goal(curr_board, puzzle):
             return curr_board
 
         for empty_cell in empty_cells(curr_board):
             for i in range(1,puzzle.size + 1):
-                child = Sudoku(puzzle.width, puzzle.height, Sudoku._copy_board(curr_board))
-                child.board[empty_cell[0]][empty_cell[1]] = i
+                child = Sudoku._copy_board(curr_board)
+                child[empty_cell[0]][empty_cell[1]] = i
                 frontier.put(child)
 
     return None
@@ -95,21 +95,21 @@ params: Takes the current puzzle as input
 Return: The puzzle board corresponding to the goal
 Note: You can modify the function definition as you see fit
 '''
-def bfs_with_prunning(puzzle):
+def bfs_with_prunning(puzzle, board):
     #Write Code here
     frontier = queue.Queue()
-    frontier.put(puzzle)
+    frontier.put(board)
 
     while not frontier.empty():
-        curr_board = frontier.get().board
+        curr_board = frontier.get()
         if test_goal(curr_board, puzzle):
             return curr_board
 
         for empty_cell in empty_cells(curr_board):
             for i in range(1,puzzle.size + 1):
-                child = Sudoku(puzzle.width, puzzle.height, Sudoku._copy_board(curr_board))
-                child.board[empty_cell[0]][empty_cell[1]] = i
-                if valid_puzzle(int(math.sqrt(child.size)), child.board):
+                child = Sudoku._copy_board(curr_board)
+                child[empty_cell[0]][empty_cell[1]] = i
+                if valid_puzzle(int(math.sqrt(len(child))), child):
                     frontier.put(child)
 
     return None
@@ -135,26 +135,32 @@ def dfs_with_prunning(puzzle):
             for i in range(1,puzzle.size + 1):
                 child = Sudoku(puzzle.width, puzzle.height, Sudoku._copy_board(curr_board))
                 child.board[empty[0]][empty[1]] = i
-                if valid_puzzle(int(math.sqrt(child.size)), child.board):
+                if valid_puzzle(int(math.sqrt(len(child.board))), child.board):
                     frontier.put(child)
-                dfs(child)
+                dfs_with_prunning(child)
     return None
 
 
 if __name__ == "__main__":
 
     
-    puzzle=Sudoku(2,2).difficulty(0.6) # Constructs a 2 x 2 puzzle
+    puzzle=Sudoku(2,2).difficulty(0.2) # Constructs a 2 x 2 puzzle
     puzzle.show_full() # Pretty prints the puzzle
     print(valid_puzzle(2,puzzle.board)) # Checks if the puzzle is valid
     print(test_goal(puzzle.board,puzzle)) # Checks if the given puzzle board is the goal for the puzzle
     print(empty_cells(puzzle.board)) # Prints the empty cells as row and column values in a list for the current puzzle board
-    # print("BFS:              ", bfs(puzzle))
-    # print("DFS:              ", dfs(puzzle))
-    # print("BFS with pruning: ", bfs_with_prunning(puzzle))
-    # print("DFS with pruning: ", dfs_with_prunning(puzzle))
-
-    # print("Time for BFS:              ", timeit.timeit('bfs(puzzle)', number=1, globals=globals()))
-    # print("Time for DFS:              ", timeit.timeit('dfs(puzzle)', number=1, globals=globals()))
-    print("Time for BFS with pruning: ", timeit.timeit('bfs_with_prunning(puzzle)', number=10, globals=globals()))
-    # print("Time for DFS with pruning: ", timeit.timeit('dfs_with_prunning(puzzle)', number=1, globals=globals()))
+    
+    # Running each searche and printing the solution along with the runtime (which is an average of 10 runs)
+    print("____________________________________________________________________________________")
+    print("BFS solution: ", bfs(puzzle, puzzle.board))
+    print("Time for BFS: ", timeit.timeit('bfs(puzzle, puzzle.board)', number=10, globals=globals()))
+    print("____________________________________________________________________________________")
+    print("DFS solution: ", dfs(puzzle))
+    print("Time for DFS: ", timeit.timeit('dfs(puzzle)', number=10, globals=globals()))
+    print("____________________________________________________________________________________")
+    print("BFS with pruning solution: ", bfs_with_prunning(puzzle, puzzle.board))
+    print("Time for BFS with pruning: ", timeit.timeit('bfs_with_prunning(puzzle, puzzle.board)', number=10, globals=globals()))
+    print("____________________________________________________________________________________")
+    print("DFS with pruning solution: ", dfs_with_prunning(puzzle))
+    print("Time for DFS with pruning: ", timeit.timeit('dfs_with_prunning(puzzle)', number=10, globals=globals()))
+    print("____________________________________________________________________________________")
